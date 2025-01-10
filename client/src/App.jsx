328a73a51fc5e028d3c990ba8dc7c47bd7add0c8
jaydipjadhav <jaydipdjadhav>
2025-01-10 19:27:50 +0530

    add styling and layout for video chat app interface

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,16 @@ import './App.css'
 function App() {
 
 
+
   return (
     <>
-      <div>
+      <div className='conatiner'>
+        <div className='box'>
+          <h1>React Video Chat App</h1>
+          <input type="text" placeholder='Enter a Eamil' />
+          <input type="text" placeholder='enter romm id' />
+          <button>Connect</button>
+        </div>
         
       </div>
       
